ad3dd816fa8f16d091eb9bca63a4635d12b69c85
Santiago Leyva <99155189+DevSanti12@users.noreply.github.com>
2025-05-25 23:43:27 -0500

    Added all digits test

--- LCDDigitsTests/LCDDigitsUnitTests.cs
+++ LCDDigitsTests/LCDDigitsUnitTests.cs
@@ -63,6 +63,18 @@ namespace LCDDigitsTests
             Assert.Throws<ArgumentException>(() => lcdDigits.GenerateLCD("9a1"));
         }
 
+        [Fact]
+        public void GenerateLCD_AllDigits_ReturnsFullLCDRepresentation()
+        {
+            var lcdDigits = new LCDDigits();
+            var result = lcdDigits.GenerateLCD("0123456789");
+            var expected =Align(
+        @"._....._.._....._.._.._.._.._.
+          |.|..|._|._||_||_.|_...||_||_|
+          |_|..||_.._|..|._||_|..||_|..|");
+            Assert.Equal(expected, result);
+        }
+
         //helper function to pretty print digits
         public static string Align(string input)
         {
