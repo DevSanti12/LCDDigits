72d1e0080f73f68a6d5ea819866fb2b007e2c8a6
Santiago Leyva <99155189+DevSanti12@users.noreply.github.com>
2025-05-25 23:30:30 -0500

    Added lcd print logic and added helper function to display tests expectations

--- LCDDigitsLibrary/LCDDigits.cs
+++ LCDDigitsLibrary/LCDDigits.cs
@@ -1,7 +1,24 @@
-﻿namespace LCDDigitsLibrary
+﻿using System.Collections.Generic;
+
+namespace LCDDigitsLibrary
 {
     public class LCDDigits
     {
+        private readonly Dictionary<char, string[]> _lcdMapped = new Dictionary<char, string[]>
+        {
+            { '0', new[] { "._.", "|.|", "|_|" } },
+            { '1', new[] { "...", "..|", "..|" } },
+            { '2', new[] { "._.", "._|", "|_." } },
+            { '3', new[] { "._.", "._|", "._|" } },
+            { '4', new[] { "...", "|_|", "..|" } },
+            { '5', new[] { "._.", "|_.", "._|" } },
+            { '6', new[] { "._.", "|_.", "|_|" } },
+            { '7', new[] { "._.", "..|", "..|" } },
+            { '8', new[] { "._.", "|_|", "|_|" } },
+            { '9', new[] { "._.", "|_|", "..|" } },
+        };
+
+
         public string GenerateLCD(string input)
         {
             if (string.IsNullOrEmpty(input))
@@ -9,7 +26,22 @@
                 return "";
             }
 
-            return null;
+            var lcdPrint = new List<string>
+            {
+                "", "", "" //place holders
+            };
+
+            foreach(var digit in input)
+            {
+                if (_lcdMapped.ContainsKey(digit))
+                {
+                    var lcdRepresentation = _lcdMapped[digit];
+                    lcdPrint[0] += lcdRepresentation[0] + "";
+                    lcdPrint[1] += lcdRepresentation[1] + "";
+                    lcdPrint[2] += lcdRepresentation[2] + "";
+                }
+            }
+            return string.Join("\n", lcdPrint).TrimEnd();
         }
     }
 }
--- LCDDigitsTests/LCDDigitsUnitTests.cs
+++ LCDDigitsTests/LCDDigitsUnitTests.cs
@@ -2,6 +2,7 @@ using LCDDigitsLibrary;
 
 namespace LCDDigitsTests
 {
+
     public class LCDDigitsUnitTests
     {
         [Fact]
@@ -15,6 +16,31 @@ namespace LCDDigitsTests
 
             // Assert
             Assert.Equal("", result);
+        }
+
+        [Fact]
+        public void GenerateLCD_SingleDigit_ReturnsLCDRepresentation()
+        {
+            // Arrange
+            var lcdDigits = new LCDDigits();
+
+            // Act
+            var result = lcdDigits.GenerateLCD("9");
+
+            // Assert
+            var expected = Align(
+            @"._.
+              |_|
+              ..|");
+
+            Assert.Equal(expected, result);
+        }
+
+
+        //helper function to pretty print digits
+        public static string Align(string input)
+        {
+            return string.Join("\n", input.Split('\n').Select(line => line.Trim()));
         }
     }
 }
